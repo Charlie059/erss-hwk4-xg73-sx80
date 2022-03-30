97743eea4cfc2dbaeb1db979834cb951c9b1d62b
XG <41360561+Charlie059@users.noreply.github.com>
2022-03-30 11:46:20 -0400

    Add mockClient

---
+++ app/src/main/java/edu/duke/ece568/MockClient.java
@@ -0,0 +1,70 @@
+/**
+ * Author: Yuxuan Yang AND Xuhui Gong
+ */
+package edu.duke.ece568;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class MockClient {
+
+  private final String host;
+  private final int portNum;
+  private final InputStream in;
+  private final OutputStream out;
+  private final OutputStreamWriter writer;
+  private final BufferedReader reader;
+  private final Socket socket;
+
+  /**
+   * Constructor of Client Class by given portNum and host
+   * 
+   * @param portNum
+   * @param host
+   * @throws IOException
+   */
+  public MockClient(int portNum, String host) throws IOException {
+    this.portNum = portNum;
+    this.host = host;
+    this.socket = new Socket(this.host, this.portNum);
+    this.in = socket.getInputStream();
+    this.out = socket.getOutputStream();
+    this.writer = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+    this.reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+  }
+
+  /**
+   * Send a JSON to the server, and it adds a \n automatically
+   * 
+   * @param msg to be send
+   * @throws IOException
+   */
+  public void sendMsg(String msg) throws IOException {
+    this.writer.write(msg + "\n");
+    this.writer.flush();
+  }
+
+  /**
+   * Receive messgae from the server
+   * 
+   * @return String
+   * @throws IOException
+   */
+  public String recvMsg() throws IOException {
+    return this.reader.readLine();
+  }
+
+  public static void main(String[] args) throws IOException {
+    MockClient mockClient = new MockClient(12345,"127.0.0.1");
+    mockClient.sendMsg("173\n" +
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<create>\n" +
+            " <account id=\"123456\" balance=\"1000\"/>\n" +
+            " <symbol sym=\"SPY\">\n" +
+            " <account id=\"123456\">100000</account>\n" +
+            " </symbol>\n" +
+            "</create> ");
+  }
+
+}
